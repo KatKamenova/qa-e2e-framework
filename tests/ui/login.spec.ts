e5bf534a328024711ad8caa4c5647be952b84f70
Katrin Kamenova <kkamenova277@gmail.com>
2026-04-22 21:11:36 +0200

    login_test

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -2,6 +2,11 @@ import { test, expect } from "@playwright/test";
 
 test("Login successfully", async ({ page }) => {
   await page.goto("https://practice.expandtesting.com/login");
+  await page.getByRole("textbox", { name: "Username" }).fill("practice");
+  await (page.getByRole("textbox", { name: "Password" }) as any).fill(
+    "SuperSecretPassword!",
+  );
+  await page.getByRole("button", { name: "Login" }).click();
 
-  await (page.locator("#username") as any).fill("admin");
+  await expect(page.locator("#flash")).toBeVisible();
 });
