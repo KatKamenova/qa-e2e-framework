d2956d1a43921d7bc11d529aed30a7010801f2ce
Katrin Kamenova <kkamenova277@gmail.com>
2026-04-22 18:01:45 +0200

    new_code

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -2,4 +2,6 @@ import { test, expect } from "@playwright/test";
 
 test("Login successfully", async ({ page }) => {
   await page.goto("https://practice.expandtesting.com/login");
+
+  await (page.locator("#username") as any).fill("admin");
 });
