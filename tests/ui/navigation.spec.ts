4862e620c3bd50cefac05b22c1c0d369ee68cf45
Katrin Kamenova <kkamenova277@gmail.com>
2026-04-24 14:16:06 +0200

    validation_test_edited

--- tests/ui/navigation.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Navigation to the login page", async ({ page }) => {
-  await page.goto("https://practice.expandtesting.com/");
-  await page.getByRole("link", { name: "Form Authentication" }).click();
-
-  await expect(page).toHaveURL("https://practice.expandtesting.com/login");
-});
--- tests/ui/validation.spec.ts
+++ tests/ui/validation.spec.ts
@@ -1,8 +1,16 @@
 import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/LoginPage";
 
-test("Username & Password validation", async ({ page }) => {
-  await page.goto("https://practice.expandtesting.com/login");
-  await page.getByRole("button", { name: "Login" }).click();
+test(
+  "Username & Password validation",
+  { tag: ["@validation", "@regression"] },
+  async ({ page }) => {
+    const loginPage = new LoginPage(page);
 
-  await expect(page.locator("#flash")).toBeVisible();
-});
+    await loginPage.goto();
+    await page.goto("https://practice.expandtesting.com/login");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.locator("#flash")).toBeVisible();
+  },
+);
